9e22259a6bdff8cd36e4d8e9aef214e8a1273afa
42Tabares <42tabares@gmail.com>
2022-09-20 14:24:39 -0500

    Arreglos a eventro de transferencia

--- domain/src/main/java/com/sofka/domain/wallet/eventos/TransferenciaExitosa.java
+++ domain/src/main/java/com/sofka/domain/wallet/eventos/TransferenciaExitosa.java
@@ -1,10 +1,35 @@
 package com.sofka.domain.wallet.eventos;
 
 import co.com.sofka.domain.generic.DomainEvent;
+import com.sofka.domain.wallet.objetosdevalor.Estado;
+import com.sofka.domain.wallet.objetosdevalor.TransferenciaID;
+import com.sofka.domain.wallet.objetosdevalor.WalletID;
 
 public class TransferenciaExitosa extends DomainEvent {
 
-    public TransferenciaExitosa(){
-        super("com.sofka.domain.wallet.TransferenciaExitosa");
-    }
+  private final WalletID walletID;
+
+  private final TransferenciaID transferenciaID;
+
+  private final Estado estadoDeTransferencia;
+
+  public TransferenciaExitosa(WalletID walletID, TransferenciaID transferenciaID,
+      Estado estadoDeTransferencia) {
+    super("com.sofka.domain.wallet.TransferenciaExitosa");
+    this.walletID = walletID;
+    this.transferenciaID = transferenciaID;
+    this.estadoDeTransferencia = estadoDeTransferencia;
+  }
+
+  public WalletID getWalletID() {
+    return walletID;
+  }
+
+  public TransferenciaID getTransferenciaID() {
+    return transferenciaID;
+  }
+
+  public Estado getEstadoDeTransferencia() {
+    return estadoDeTransferencia;
+  }
 }
--- domain/src/main/java/com/sofka/domain/wallet/eventos/TransferenciaFallida.java
+++ domain/src/main/java/com/sofka/domain/wallet/eventos/TransferenciaFallida.java
@@ -1,10 +1,35 @@
 package com.sofka.domain.wallet.eventos;
 
 import co.com.sofka.domain.generic.DomainEvent;
+import com.sofka.domain.wallet.objetosdevalor.Estado;
+import com.sofka.domain.wallet.objetosdevalor.TransferenciaID;
+import com.sofka.domain.wallet.objetosdevalor.WalletID;
 
 public class TransferenciaFallida extends DomainEvent {
 
-    public TransferenciaFallida() {
-        super("com.sofka.domain.wallet.TransferenciaFallida");
-    }
+  private final WalletID walletID;
+
+  private final TransferenciaID transferenciaID;
+
+  private final Estado estadoDeTransferencia;
+
+  public TransferenciaFallida(WalletID walletID, TransferenciaID transferenciaID,
+      Estado estadoDeTransferencia) {
+    super("com.sofka.domain.wallet.TransferenciaFallida");
+    this.walletID = walletID;
+    this.transferenciaID = transferenciaID;
+    this.estadoDeTransferencia = estadoDeTransferencia;
+  }
+
+  public WalletID getWalletID() {
+    return walletID;
+  }
+
+  public TransferenciaID getTransferenciaID() {
+    return transferenciaID;
+  }
+
+  public Estado getEstadoDeTransferencia() {
+    return estadoDeTransferencia;
+  }
 }
