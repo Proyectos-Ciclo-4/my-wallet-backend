e2aacf1363ebe19d9a29bbe4d2aa595d3b5c3978
42Tabares <42tabares@gmail.com>
2022-09-20 15:53:58 -0500

    Añadidos apply events en WalletChange

--- domain/src/main/java/com/sofka/domain/wallet/Wallet.java
+++ domain/src/main/java/com/sofka/domain/wallet/Wallet.java
@@ -27,8 +27,6 @@ import java.util.Optional;
 
 public class Wallet extends AggregateEvent<WalletID> {
 
-  protected UsuarioID usuarioID;
-
   protected Saldo saldo;
 
   protected List<Motivo> motivos;
