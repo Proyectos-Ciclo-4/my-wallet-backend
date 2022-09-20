c9463f6cf5de5541bd67e721a24115119c11842e
Luisa Fernanda Barragan Avila <luisaavila304@gmail.com>
2022-09-20 15:36:09 -0500

    Caso de Uso crearWallet

--- business/src/main/java/com/sofka/business/usecase/crearWalletUseCase.java
+++ business/src/main/java/com/sofka/business/usecase/crearWalletUseCase.java
@@ -1,5 +1,26 @@
 package com.sofka.business.usecase;
 
-public class crearWalletUseCase extends UseCaseForCommand {
+import co.com.sofka.domain.generic.DomainEvent;
+import com.sofka.domain.wallet.Usuario;
+import com.sofka.domain.wallet.Wallet;
+import com.sofka.domain.wallet.comandos.CrearWallet;
+import com.sofka.domain.wallet.objetosdevalor.UsuarioID;
+import com.sofka.domain.wallet.objetosdevalor.WalletID;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+public class crearWalletUseCase extends UseCaseForCommand<CrearWallet> {
+
+
+  @Override
+  public Flux<DomainEvent> apply(Mono<CrearWallet> crearWallet) {
+    return crearWallet.flatMapMany(command -> {
+
+      var wallet = new Wallet(command.getWalletID(), command.getUsuarioID(), command.getSaldo(),
+          command.getMotivos());
+      // wallet.asignarUsuario();
+      return Flux.fromIterable(wallet.getUncommittedChanges());
+
+    });
+  }
 }
