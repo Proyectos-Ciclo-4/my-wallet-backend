14181a1fb958da97848d557e570fb11337cf8847
Jd4rc3 <jdarce91@misena.edu.co>
2022-09-23 15:55:43 -0500

    Wallet creada funcionando con el front

--- business/src/main/java/com/sofka/business/usecase/CrearWalletUseCase.java
+++ business/src/main/java/com/sofka/business/usecase/CrearWalletUseCase.java
@@ -4,6 +4,7 @@ import co.com.sofka.domain.generic.DomainEvent;
 import com.sofka.business.usecase.gateway.UsuarioRepositorio;
 import com.sofka.domain.wallet.Wallet;
 import com.sofka.domain.wallet.comandos.CrearWallet;
+import com.sofka.domain.wallet.objetosdevalor.Nombre;
 import com.sofka.domain.wallet.objetosdevalor.Saldo;
 import com.sofka.domain.wallet.objetosdevalor.WalletID;
 import reactor.core.publisher.Flux;
@@ -11,31 +12,41 @@ import reactor.core.publisher.Mono;
 
 public class CrearWalletUseCase extends UseCaseForCommand<CrearWallet> {
 
-  private final UsuarioRepositorio usuarioRepositorio;
-
-  public CrearWalletUseCase(UsuarioRepositorio usuarioRepositorio) {
-    this.usuarioRepositorio = usuarioRepositorio;
-  }
-
   @Override
   public Flux<DomainEvent> apply(Mono<CrearWallet> crearWallet) {
     return crearWallet.flatMapMany(command -> {
-      var usuario = usuarioRepositorio.obtenerDatosUsuario(command.getEmail().value(),
-          command.getTelefono().value()).block();
-
-      if (usuario == null) {
-        var wallet = new Wallet(WalletID.of(""));
-        wallet.rechazarCreacion(command.getUsuarioID().value());
-
-        return Flux.fromIterable(wallet.getUncommittedChanges());
-      }
-
       var wallet = new Wallet(command.getUsuarioID(), new Saldo(100.00));
 
-      wallet.asignarUsuario(command.getUsuarioID(), usuario.getNombre(), usuario.getEmail(),
-          usuario.getTelefono());
+      wallet.asignarUsuario(command.getUsuarioID(), new Nombre("nombre"), command.getEmail(),
+          command.getTelefono());
 
       return Flux.fromIterable(wallet.getUncommittedChanges());
     });
   }
+
+
+
+  /*{
+    return crearWallet.flatMapMany(command -> usuarioRepositorio.obtenerDatosUsuario(command.getEmail().value(),
+        command.getTelefono().value()).flatMapMany((usuarioData)-> {
+          System.out.println("usuarioData = " + usuarioData);
+
+          if (usuarioData != null) {
+            var wallet = new Wallet(WalletID.of(""));
+            wallet.rechazarCreacion(usuarioData.identity().value());
+            wallet.rechazarCreacion(usuarioData.identity().value());
+
+            return Flux.fromIterable(wallet.getUncommittedChanges());
+          }
+
+          var wallet = new Wallet(command.getUsuarioID(), new Saldo(100.00));
+
+          wallet.asignarUsuario(command.getUsuarioID(), usuarioData.getNombre(),
+              usuarioData.getEmail(),
+              usuarioData.getTelefono());
+
+          return Flux.fromIterable(wallet.getUncommittedChanges());
+        }
+    ));
+  }*/
 }
