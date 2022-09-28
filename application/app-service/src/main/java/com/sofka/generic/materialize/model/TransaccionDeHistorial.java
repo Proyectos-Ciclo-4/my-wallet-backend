e464829260c28d16056347830cf23cdca84bce90
42Tabares <42tabares@gmail.com>
2022-09-28 17:04:57 -0500

    Fixed bug on Transaccion de historial model

--- application/app-service/src/main/java/com/sofka/generic/materialize/model/TransaccionDeHistorial.java
+++ application/app-service/src/main/java/com/sofka/generic/materialize/model/TransaccionDeHistorial.java
@@ -1,6 +1,8 @@
 package com.sofka.generic.materialize.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
@@ -12,12 +14,23 @@ public class TransaccionDeHistorial {
 
   private String estado;
 
-  private String motivo;
+  private Motivo motivo;
 
   private Double valor;
 
   private String destino;
 
   private String transferencia_id;
+
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class Motivo {
+
+    private String descripcion;
+
+    private String color;
+
+  }
 
 }
